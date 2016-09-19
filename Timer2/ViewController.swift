1d2b9da6add6552aae71e66df60e4cfdcf0bfe38
Lewis Lebentz <LewisLebentz@users.noreply.github.com>
2016-09-19 22:25:51 +0100

    Initial Commit

---
+++ Timer2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timer2
+//
+//  Created by Lewis Lebentz on 19/09/2016.
+//  Copyright © 2016 Lewis Lebentz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
